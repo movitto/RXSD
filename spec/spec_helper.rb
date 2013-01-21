8702b0b1a09c2a5e834084eac8dc5318d2ad6cf0
Vasilij Melnychuk <isqrel@gmail.com>
2013-01-21 20:20:31 +0200

    Update gems to newer version Move gem info to .gemspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,7 @@
 # Copyright (C) 2010 Mohammed Morsi <movitto@yahoo.com>
 # Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
 
-require 'rubygems'
-require 'spec'
+require 'rspec'
 
 CURRENT_DIR=File.dirname(__FILE__)
 $: << File.expand_path(CURRENT_DIR + "/../lib")
